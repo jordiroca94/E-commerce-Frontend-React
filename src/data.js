0303c802ca251a080fac6e1923892121fc92a3ce
Jordi Roca <jordirocasoler94@gmail.com>
2021-12-29 19:45:03 +0100

    Slider done

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const sliderItems = [
+    {
+        id:1, 
+        img: "https://images.unsplash.com/photo-1599751449029-e4ea8f21d966?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+        title:"WINTER SALES",
+        desc: "DONT LET THE COLD BE YOUR EXCUSE!",
+        bg:"f5fafd"
+    },
+    {
+        id:2, 
+        img: "https://images.unsplash.com/photo-1606525380696-c2bfe72f7b8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        title:"SPRING & AUTUMN",
+        desc: "PERFECT WEATHER TO GO FOR A WALK",
+        bg:"fcf1ed"
+    },
+    {
+        id:3, 
+        img: "https://images.unsplash.com/photo-1489633908075-1c914e8ee5ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        title:"SUMMER SALES",
+        desc: "IT IS NOT TOO HOT, LET'S GO FOR A SWIM",
+        bg:"f5fafd"
+    },
+
+]

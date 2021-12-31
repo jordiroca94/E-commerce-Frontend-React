5dae7dd4e68e2ebe9a6c7f0f5a1c40426ff2f4ee
Jordi Roca <jordirocasoler94@gmail.com>
2021-12-31 14:10:02 +0100

    mobile responsive done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,124 +1,134 @@
-import { Room, Facebook, Instagram, LinkedIn, MailOutline, Phone, Twitter } from "@material-ui/icons";
+import {
+  Room,
+  Facebook,
+  Instagram,
+  LinkedIn,
+  MailOutline,
+  Phone,
+  Twitter,
+} from "@material-ui/icons";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
-display:flex;
+  display: flex;
+  ${mobile({flexDirection: "column"})}; 
 `;
 const Left = styled.div`
-flex:1;
-display:flex; 
-flex-direction:column; 
-padding:20px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
 `;
-const Logo = styled.h1``
+const Logo = styled.h1``;
 const Desc = styled.p`
-margin:20px 0px; 
-`
+  margin: 20px 0px;
+`;
 const SocialContainer = styled.h1`
-display:flex;
-`
+  display: flex;
+`;
 const SocialIcon = styled.h1`
-width:40px; 
-height:40px; 
-border-radius:50%;
-color:white;
-background-color: #${props =>props.color};
-display:flex; 
-align-items:center;
-justify-content:center;
-margin-right:20px; 
-
-`
-
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
 
 const Center = styled.div`
-flex:1;
-padding:20px;
+  flex: 1;
+  padding: 20px;
+  ${mobile({display: "none"})}; 
 `;
 
 const Title = styled.h3`
-margin-bottom:30px;
+  margin-bottom: 30px;
 `;
 
 const List = styled.ul`
-margin:0; 
-padding:0; 
-list-style:none; 
-display:flex;
-flex-wrap:wrap; 
-`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 const ListItem = styled.li`
-width:50%; 
-margin-bottom:10px;
-`
+  width: 50%;
+  margin-bottom: 10px;
+`;
 
 const Right = styled.div`
-flex:1;
-padding:20px;
+  flex: 1;
+  padding: 20px;
+  ${mobile({backgroundColor: "#fff8f8"})}; 
 `;
 
 const ContactItem = styled.div`
-margin-bottom:20px; 
-display:flex; 
-align-items:center;
-`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
 
 const CreatedBy = styled.p`
-margin-bottom:20px;
-`
-
-
-
+  margin-bottom: 20px;
+`;
 
 const Footer = () => {
-    return (
-        <Container>
-            <Left>
-            <Logo>MY-WAY</Logo>
-            <Desc>MY-Way is your online sport Shop  </Desc>
-            <CreatedBy>Created By <b>Jordi Roca Soler</b></CreatedBy>
-            <SocialContainer>
-                <SocialIcon color ="3B5999">
-                    <Facebook/>
-                </SocialIcon>
-                <SocialIcon color ="E4405F">
-                    <Instagram/>
-                </SocialIcon>
-                <SocialIcon color ="55ACEE">
-                    <Twitter/>
-                </SocialIcon>
-                <SocialIcon color ="3B5999">
-                    <LinkedIn/>
-                </SocialIcon>
-            </SocialContainer>
-            </Left>
-            <Center>
-                <Title>Useful Links</Title>
-                <List>
-                    <ListItem>Home</ListItem>
-                    <ListItem>Cart</ListItem>
-                    <ListItem>My Account</ListItem>
-                    <ListItem>Wishlist</ListItem>
-                    <ListItem>Terms</ListItem>
-                </List>
-            </Center>
-            <Right>
-<Title>Contact</Title>
-<ContactItem><Room style={{marginRight:"10px"}}/>
-    Nou de la Rambla 27, Barcelona 08000
-</ContactItem>
-<ContactItem><Phone style={{marginRight:"10px"}}/>
-    +34 622 588 996
-</ContactItem>
-<ContactItem><MailOutline style={{marginRight:"10px"}}/>
-    jordirocasoler94@gmail.com
-</ContactItem>
+  return (
+    <Container>
+      <Left>
+        <Logo>MY-WAY</Logo>
+        <Desc>MY-Way is your online sport Shop </Desc>
+        <CreatedBy>
+          Created By <b>Jordi Roca Soler</b>
+        </CreatedBy>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="3B5999">
+            <LinkedIn />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} />
+          Nou de la Rambla 27, Barcelona 08000
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} />
+          +34 622 588 996
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} />
+          jordirocasoler94@gmail.com
+        </ContactItem>
+      </Right>
+    </Container>
+  );
+};
 
-
-            </Right>
-        </Container>
-    )
-}
-
-export default Footer
+export default Footer;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 const Title = styled.h1`
@@ -15,17 +16,20 @@ const FilterContainer = styled.div`
 `;
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })};
 `;
 
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
-  margin-right:20px;
+  margin-right: 20px; 
+  ${mobile({marginRight: "0px"})}; 
 `;
 
 const Select = styled.select`
-padding:10px; 
-margin-right:20px; 
+  padding: 10px;
+  margin-right: 20px;
+  ${mobile({margin: "10px 0px"})}; 
 `;
 const Option = styled.option``;
 
@@ -39,30 +43,33 @@ const ProductList = () => {
         <Filter>
           <FilterText>Filter Products:</FilterText>
           <Select>
-              <Option disabled selected>Color</Option>
-              <Option>White</Option>
-              <Option>Black</Option>
-              <Option>Red</Option>
-              <Option>Blue</Option>
-              <Option>Yellow</Option>
-              <Option>Green</Option>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
           </Select>
           <Select>
-              <Option disabled selected>Size</Option>
-              <Option>XS</Option>
-              <Option>S</Option>
-              <Option>M</Option>
-              <Option>L</Option>
-              <Option>XL</Option>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
           </Select>
         </Filter>
         <Filter>
           <FilterText>Sort Products:</FilterText>
           <Select>
-              <Option selected>Newest</Option>
-              <Option>Price (asc)</Option>
-              <Option>Price (desc)</Option>
-
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
           </Select>
         </Filter>
       </FilterContainer>
